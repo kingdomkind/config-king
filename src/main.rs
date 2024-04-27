1e03ebaf3f0fcc349b50c68a5df0647d32f42b2f
kingdomkind <pikadevs@proton.me>
2024-04-27 14:30:48 +0100

    VM

--- src/main.rs
+++ src/main.rs
@@ -68,10 +68,9 @@ fn main() -> Result<(), mlua::Error> {
         }
     }
 
-    /*
     for value in &packages {
-        println!("{}", value);
-    } */
+        println!("I want to remove {}", value);
+    }
 
     Ok(())
 }
