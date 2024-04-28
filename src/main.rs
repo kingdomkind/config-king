3ba65b0586563dc87283955999508a6d16a84908
kingdomkind <pikadevs@proton.me>
2024-04-28 11:38:00 +0100

    vm

--- src/main.rs
+++ src/main.rs
@@ -79,7 +79,7 @@ fn main() -> Result<(), mlua::Error> {
     dep.arg("-D");
 
     for value in &packages {
-        println!(value);
+        println!("{}", value);
         output.arg(value);
         dep.arg(value);
     }
