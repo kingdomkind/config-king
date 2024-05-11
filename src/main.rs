4d5358a694151d8afd3eb47b9ba16a1ed2140646
kingdomkind <pikadevs@proton.me>
2024-05-11 21:09:41 +0100

    a

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,23 @@ fn logger(to_print: &'static str, log_level: &'static str) {
     let array = ["juan"];
 } */
 
-fn send_output(mut output : Command) -> bool{
-    let spawned = output.spawn().expect("Unable to output command");
-    let reader = BufReader::new(spawned.stdout.expect("Failed to capture stdout"));
+fn send_output(mut output : Command) -> bool {
+    let mut spawned = output.spawn().expect("Unable to output command");
 
-    for line in reader.lines() {
-        println!("{}", line.expect("Failed to read line"));
+    if let Some(ref mut stdout) = spawned.stdout {
+        let reader = BufReader::new(stdout);
+
+        for line in reader.lines() {
+            println!("{}", line.expect("Failed to read line"));
+        }
+    } else {
+        println!("No stdout available");
     }
 
-    //let wait = spawned.wait().expect("Failed to wait for output to end");
-    //return  wait.success();
-    true
+    let wait = spawned.wait().expect("Failed to wait for output to end");
+    return wait.success();
 }
+
 
 fn build_aur(name : &str) {
     println!("Building (AUR) {}...", name);
