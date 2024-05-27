0b465ac2f205d7de0bc454ad20e8ce4b57658b94
kingdomkind <pikadevs@proton.me>
2024-05-27 19:50:57 +0100

    a

--- src/config.lua
+++ src/config.lua
@@ -25,7 +25,7 @@ Packages = {
 
 Symlinks = {
     ["/home/pika/test2"] = "/home/pika/config-king",
-    ["/home/pika/test3"] = "/home/pika/config-king"
+    ["/home/pika/test3"] = "/home/pika/config-king",
     ["/home/pika/test4"] = "/home/pika/config-king",
 }
 
